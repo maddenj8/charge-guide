f57f0fa16a91bfe66abe0dbccc1ad5a5447a36cb
maddenj8 <jason.madden8@mail.dcu.ie>
2018-01-24 20:27:12 +0000

    Added a Nav Drawer

--- app/src/main/java/com/dcuproject/jmadden/chargeguide/MapMain.java
+++ app/src/main/java/com/dcuproject/jmadden/chargeguide/MapMain.java
@@ -2,11 +2,20 @@ package com.dcuproject.jmadden.chargeguide;
 
 import android.location.Address;
 import android.location.Geocoder;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -17,7 +26,7 @@ import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.util.ArrayList;
 
-public class MapMain extends FragmentActivity implements OnMapReadyCallback {
+public class MapMain extends FragmentActivity implements OnMapReadyCallback, NavigationView.OnNavigationItemSelectedListener {
 
     private GoogleMap mMap;
 
@@ -29,9 +38,15 @@ public class MapMain extends FragmentActivity implements OnMapReadyCallback {
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-    }
 
-    public void showLocation() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
 
     }
 
@@ -44,6 +59,7 @@ public class MapMain extends FragmentActivity implements OnMapReadyCallback {
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
      */
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
@@ -51,25 +67,89 @@ public class MapMain extends FragmentActivity implements OnMapReadyCallback {
         // Add a marker in Sydney and move the camera
         LatLng ireland = new LatLng(53.433333, -7.95);
         //mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(ireland, 6.0f));
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(ireland, 6.5f));
     }
 
     public void showLocation(View view) {
         EditText edit = (EditText)findViewById(R.id.search_query);
         String location = edit.getText().toString();
+        location += " Ireland";
+        Boolean found = false;
         Geocoder coder = new Geocoder(this);
         try {
-            ArrayList<Address> locations = (ArrayList<Address>) coder.getFromLocationName(location, 50);
+            ArrayList<Address> locations = (ArrayList<Address>) coder.getFromLocationName(location, 100);
+            Log.i("LENGTH OF ADDRESSES", location.toString());
             for (Address add : locations) {
                 Log.i("COUNTRYNAME", add.getCountryName());
-                if (add.getCountryName() == "Ireland") {
-                    mMap.animateCamera(CameraUpdateFactory.newLatLng(new LatLng(locations.get(0).getLatitude(), locations.get(0).getLongitude())));
+                if (add.getCountryName().equals("Ireland")) {
+                    Log.i("COUNTRYNAME2", add.getCountryName());
+                    mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(locations.get(0).getLatitude(), locations.get(0).getLongitude()), 8.0f));
+                    found = true;
                     break;
                 }
+            }
+            if (!found) {
+                Toast.makeText(this, location + " is not found in Ireland please try again.", Toast.LENGTH_SHORT);
             }
         } catch (Exception E) {
             E.printStackTrace();
         }
     }
 
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.nav_bar, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camera) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
 }
