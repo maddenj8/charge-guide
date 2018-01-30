96ed3ed989b2db9c78f7423cc1362ee210d880a0
maddenj8 <jason.madden8@mail.dcu.ie>
2018-01-30 21:45:16 +0000

    trying to get user's location (not working at the minute)

--- app/src/main/java/com/dcuproject/jmadden/chargeguide/first_launch.java
+++ app/src/main/java/com/dcuproject/jmadden/chargeguide/first_launch.java
@@ -1,6 +1,9 @@
 package com.dcuproject.jmadden.chargeguide;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -11,28 +14,34 @@ import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.places.GeoDataClient;
+import com.google.android.gms.location.places.PlaceDetectionClient;
+import com.google.android.gms.location.places.Places;
 
 
 public class first_launch extends AppCompatActivity {
+    private static final int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1;
     private Spinner makes, model;
     private Button currentLocAsHome;
     private Button fin;
-
-
-
+    private Boolean home;
+    boolean mLocationPermissionGranted;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first_launch);
 
-
-
         makes = (Spinner) findViewById(R.id.make_dropdown);
         model = (Spinner) findViewById(R.id.model_dropdown);
         currentLocAsHome = (Button) findViewById(R.id.currentLocAsHome);
         fin = (Button) findViewById(R.id.enterApp);
 
+        GeoDataClient nGeoDataClient = Places.getGeoDataClient(this, null);
+        PlaceDetectionClient mPlaceDetectionClient = Places.getPlaceDetectionClient(this, null);
+        FusedLocationProviderClient mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
 
         makes.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
@@ -70,48 +79,33 @@ public class first_launch extends AppCompatActivity {
 
         model.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
-
             public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
                 SharedPreferences sharedPref = getApplicationContext().getSharedPreferences("userPref", MODE_PRIVATE);
                 SharedPreferences.Editor editor = sharedPref.edit();
                 String selected_make = model.getSelectedItem().toString();
                 editor.putString("selectedMake", selected_make);
                 editor.apply();
-
-
             }
-
 
             @Override
             public void onNothingSelected(AdapterView<?> parentView) {
-
             }
-
-
         });
 
-
-
-
-/*
-
-          currentLocAsHome.setOnClickListener(new View.OnClickListener(){
+        currentLocAsHome.setOnClickListener(new View.OnClickListener(){
             public void onClick (View view){
-            SharedPreferences sharedPref = getApplicationContext().getSharedPreferences("userPref", MODE_PRIVATE);
-            SharedPreferences.Editor editor = sharedPref.edit();
-            Boolean home = currentLocAsHome.isActivated();
-
-
-
-
-        }
+                //SharedPreferences sharedPref = getApplicationContext().getSharedPreferences("userPref", MODE_PRIVATE);
+                //SharedPreferences.Editor editor = sharedPref.edit();
+                home = currentLocAsHome.isActivated(); //check if the checkbox is ticked
+            }
         });
- */
-
 
         fin.setOnClickListener(new View.OnClickListener() {
 
             public void onClick(View view) {
+                if (home) { // if the user wants the current location as home
+                    //get the users current location
+                }
                 startActivity(new Intent(first_launch.this, MapMain.class));
                 SharedPreferences sharedPref = getApplicationContext().getSharedPreferences("userPref", MODE_PRIVATE);
                 SharedPreferences.Editor editor = sharedPref.edit();
@@ -121,17 +115,38 @@ public class first_launch extends AppCompatActivity {
 
 
         });
-
+        //getLocationPermission();
     }
 
+    private void getLocationPermission() {
+    /*
+     * Request location permission, so that we can get the location of the
+     * device. The result of the permission request is handled by a callback,
+     * onRequestPermissionsResult.
+     */
+        if (ContextCompat.checkSelfPermission(this.getApplicationContext(), android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            mLocationPermissionGranted = true;
 
+        } else {
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
+                    PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+        }
+    }
 
-
-
-
-
-
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        mLocationPermissionGranted = false;
+        switch (requestCode) {
+            case PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    mLocationPermissionGranted = true;
+                }
             }
+        }
+        //getUsersLocation();
+    }
+}
 
 
 
