d55bef0579c49c13e45a1dbe68edb13f953fa579
Conor Nugent <conor.nugent12@mail.dcu.ie>
2018-02-01 15:20:47 +0000

    Finished the kml parser

--- app/kmlparser.py
+++ app/kmlparser.py
@@ -1,4 +1,4 @@
-all_chargers = open("charging-locations (4).kml" , "r") 
+all_chargers = open("charging-locations.kml" , "r") 
 ac43 = open("ac43.kml" , "r+")
 chademo = open("chademo.kml" , "r+")
 ccs = open("ccs.kml" , "r+" ) 
@@ -6,8 +6,6 @@ ccs = open("ccs.kml" , "r+" )
 acpart = False
 ccspart = False
 chademopart = False
-i = 0 
-a = ""
 text= all_chargers.readlines()
 for line in text: 
 
@@ -22,22 +20,23 @@ for line in text:
 
     if "<!-- ======== CHAdeMO ======= -->" in line :
         chademopart = True
-        ccpart = False
+        ccspart = False
         acpart = False
         
-
-    if acpart == True:
-        ac43.write(line)         
-        
-    
     if ccspart == True:
         ccs.write(line)         
        
-    if chademopart == True:
+
+    elif acpart == True:
+        ac43.write(line)         
+        
+    
+    elif chademopart == True:
         chademo.write(line)         
 
 ac43.close()
-
+ccs.close()
+chademo.close()
 
 def getname(file_name ):
 
@@ -62,4 +61,110 @@ def getname(file_name ):
     return namelist
 
 
+
+def getlatlon( file_name):
+
+    namelist = []
+    name = ""
+    charger_file = open( file_name , "r")
+
+    text = charger_file.readlines()
+
+    text = text[1:]
+    for line in text:
+        word = line.split(">")
+        if len(word) >=9:
+            name = word[2]
+            if len(word) > 9:
+               # print(word[8]  )
+                name = word[8]
+                end = (name[len(name) - 3:] )
+                if end == "tes":
+                        namelist.append(name)
+                
+    
+    charger_file.close()
+    return  namelist
+
+def getStateAC( ):
+
+    namelist = []
+    name = ""
+    charger_file = open( "ac43.kml" , "r")
+
+    text = charger_file.readlines()
+    nextlineget = False
+    text = text[1:]
+    for line in text:
+        if nextlineget == True:
+            line = line.split("<")
+            state = line[0]
+            state = state.split(" " ) 
+            state.pop()
+            line = ' '.join(state)
+            namelist.append(line)
+            nextlineget = False
+
+        if line[:3] == "<Pl":
+            nextlineget = True 
+    
+    return namelist
+
+
+def getStateChademo():
+
+
+    namelist = []
+    name = ""
+    charger_file =open( "chademo.kml" , "r")
+
+    text = charger_file.readlines()
+    nextlineget = False
+    text = text[1:]
+    for line in text:
+        if nextlineget == True:
+            line = line.split("<")
+            state = line[0]
+            state = state.strip(",")
+            state = state.strip(" ")
+            state = state.split(" " ) 
+            if len (state) >4:
+                state.pop()
+            line = ' '.join(state)
+            namelist.append(line)
+            nextlineget = False
+
+        if line[:3] == "<Pl":
+            nextlineget = True 
+    
+    return namelist
+
+
+
+
+def getStateCcs():
+    namelist = []
+    name = ""
+    charger_file =open( "ccs.kml" , "r")
+
+    text = charger_file.readlines()
+    nextlineget = False
+    text = text[1:]
+    for line in text:
+        if nextlineget == True:
+            line = line.split("<")
+            state = line[0]
+            state = state.strip(",")
+            state = state.strip(" ")
+            state = state.split(" " ) 
+            if len (state) >4:
+                state.pop()
+            line = ' '.join(state)
+            namelist.append(line)
+            nextlineget = False
+
+        if line[:3] == "<Pl":
+            nextlineget = True 
+    
+    return namelist
 
