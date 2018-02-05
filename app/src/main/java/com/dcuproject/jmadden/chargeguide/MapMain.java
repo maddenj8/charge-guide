5100289ee960edb302b6756d6923e67b48b5fede
159conor <conornugent96@gmail.com>
2018-02-05 00:07:04 +0000

    partly implemented help page

--- app/src/main/java/com/dcuproject/jmadden/chargeguide/MapMain.java
+++ app/src/main/java/com/dcuproject/jmadden/chargeguide/MapMain.java
@@ -1,24 +1,24 @@
 package com.dcuproject.jmadden.chargeguide;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.location.Address;
 import android.location.Geocoder;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
-import android.support.v4.app.FragmentManager;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Gravity;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.Toast;
@@ -41,11 +41,20 @@ public class MapMain extends FragmentActivity implements OnMapReadyCallback, Nav
     private GoogleMap mMap;
     private ImageButton hamburger;
     private PlaceAutocompleteFragment autocompleteFragment;
+    private SharedPreferences user_info;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map_main);
+
+        SharedPreferences sharedPref = getApplicationContext().getSharedPreferences("userPref", MODE_PRIVATE);
+        String config = sharedPref.getString("Setup_complete", "false");
+
+        if (!config.equals("true")) {
+            startActivity(new Intent(getApplicationContext(), first_launch.class));
+        }
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
@@ -53,21 +62,23 @@ public class MapMain extends FragmentActivity implements OnMapReadyCallback, Nav
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
 
+
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
+
         toggle.syncState();
+
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
-
         hamburger = (ImageButton) (findViewById(R.id.hamburger));
         hamburger.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 DrawerLayout drawer = (DrawerLayout) (findViewById(R.id.drawer_layout));
-                drawer.openDrawer(Gravity.START);
 
+                drawer.openDrawer(Gravity.START);
             }
         });
 
@@ -87,24 +98,22 @@ public class MapMain extends FragmentActivity implements OnMapReadyCallback, Nav
         });
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
+
+
+
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
+        user_info = getApplicationContext().getSharedPreferences("user_location", Context.MODE_PRIVATE);
+        Float user_lat = user_info.getFloat("latitude", 9999); // 9999 is to make sure the value returned when there is no value set is not
+        Float user_long = user_info.getFloat("longitude", 9999); // mistaken for a coordinate (example if -1 was used for error it's also a coordinate)
         // Add a marker in Sydney and move the camera
         LatLng ireland = new LatLng(53.433333, -7.95);
-        //mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(ireland, 6.5f));
+        LatLng userLocation = new LatLng(user_lat, user_long);
+        Log.i("USER LOCATION", user_lat.toString() + " " + user_long.toString());
+        mMap.addMarker(new MarkerOptions().position(userLocation).title("Home")); // set a marker for user location
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(ireland, 6.5f)); //animate camera towards Ireland
     }
 
     @Override
@@ -133,7 +142,6 @@ public class MapMain extends FragmentActivity implements OnMapReadyCallback, Nav
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
-
             return true;
         }
 
@@ -141,31 +149,37 @@ public class MapMain extends FragmentActivity implements OnMapReadyCallback, Nav
     }
 
 
-
-    @SuppressWarnings("StatementWithEmptyBody")
+  @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
+        item.setChecked(true);
         int id = item.getItemId();
 
-        android.app.FragmentManager fragmentManager = getFragmentManager();
 
 
-        if (id == R.id.nav_help) {
 
-        }
-       else if (id == R.id.nav_manage) {
-            Intent intent = new Intent(this, first_launch.class);
+
+        if (id == R.id.nav_manage) {
+
+            Intent intent = new Intent(getApplicationContext() , first_launch.class);
             startActivity(intent);
 
 
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.nav_help) {
+            Intent intent = new Intent(getApplicationContext() , help.class);
+            startActivity(intent);
 
+        } else if (id == R.id.nav_share) {
         }
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
-        return true;
+
+            DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+            drawer.closeDrawer(GravityCompat.START);
+            return true;
+
+
+        }
     }
-}
+
--- app/src/main/java/com/dcuproject/jmadden/chargeguide/help.java
+++ app/src/main/java/com/dcuproject/jmadden/chargeguide/help.java
@@ -1,29 +1,13 @@
 package com.dcuproject.jmadden.chargeguide;
 
-import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.os.Bundle;
 
 public class help extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_help);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        setContentView(R.layout.activity_help2);
     }
-
 }
