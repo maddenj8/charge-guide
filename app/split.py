87e92445fbfd557906cecd721dd3cb692ec5123e
Conor Nugent <conor.nugent12@mail.dcu.ie>
2018-02-05 23:14:56 +0000

    splitting it into two fixes a nasty bug for some reason unknown to logic and reasoning

---
+++ app/split.py
@@ -0,0 +1,45 @@
+all_chargers = open("charging-locations.kml" , "r") 
+ac43 = open("ac43.kml" , "r+")
+chademo = open("chademo.kml" , "r+")
+ccs = open("ccs.kml" , "r+" ) 
+
+acpart = False
+ccspart = False
+chademopart = False
+text= all_chargers.readlines()
+
+for line in text: 
+
+
+    if  "<!-- ======== AC43 ======= -->" in line :
+        ac43.write(line)
+        acpart = True
+        
+             
+     
+
+    if "<!-- ======== CHAdeMO ======= --> " in line :
+        chademo.write(line)
+        chademopart = True
+        acpart = False
+   
+    if "<!-- ======== ComboCCS ======= -->" in line:
+        ccs.write(line)
+        ccspart = True
+        acpart = False
+
+   
+    if acpart == True:
+        ac43.write(line)         
+        
+    elif ccspart == True :
+        chademopart = False
+        ccs.write(line)         
+        if  "<!-- ======== CHAdeMO ======= -->" in line :
+            ccspart = False
+            ccs.close()
+            
+
+    elif  chademopart == True:
+        ccspart = False
+        chademo.write(line)        
