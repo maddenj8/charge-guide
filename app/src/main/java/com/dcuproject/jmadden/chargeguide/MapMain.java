2e5e869679a8629e6f7c9d5999b8a944bafc3313
nugenc12 <conor.nugent12@mail.dcu.ie>
2018-02-18 21:50:40 +0000

    Pins are now on the map

--- app/src/main/java/com/dcuproject/jmadden/chargeguide/MapMain.java
+++ app/src/main/java/com/dcuproject/jmadden/chargeguide/MapMain.java
@@ -40,8 +40,10 @@ import com.google.android.gms.maps.model.MarkerOptions;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 
@@ -195,35 +197,64 @@ public class MapMain extends FragmentActivity implements OnMapReadyCallback, Nav
         return true;
         }
 
+//Toast.makeText(getApplicationContext(),"Somthing bad happended" , Toast.LENGTH_LONG).show();
 
-  // /src/chademo_output.txt
+//Toast.makeText(getApplicationContext(),"Somthing bad happended" , Toast.LENGTH_LONG).show();
+
+    // /app/src/chademo_output.txt");
+    // mMap.addMarker(new MarkerOptions().position(userLocation).title("Home"))
+
     public void pinDrop (String plug) {
-       // mMap.addMarker(new MarkerOptions().position(userLocation).title("Home")); // set a marker for user location
-        FileInputStream is;
-        BufferedReader reader;
-        ArrayList chargers = new ArrayList();
+
+
         if (plug.equals("Chademo")){
+            BufferedReader reader;
 
-            final File file = new File("/src/chademo_output.txt");
-            try {
-                is = new FileInputStream(file);
-                reader = new BufferedReader(new InputStreamReader(is));
+            try{
+                final InputStream file = getAssets().open("chademo_output.txt");
+                reader = new BufferedReader(new InputStreamReader(file));
                 String line = reader.readLine();
-
                 while(line != null){
-                    Log.d("StackOverflow", line);
+
+                    String [] charger_Info = line.split("\\|"); //
+                    String charger_Name = charger_Info[0];
+                    String latlon = charger_Info[1];
+                    String state = charger_Info[2];
+                    char stateEnds = state.charAt(state.length() -2);
+
+                    String [] split_Lat_Lon = latlon.split(",");
+                    double charger_lat = Double.parseDouble(split_Lat_Lon[0]);
+                    double charger_lon = Double.parseDouble(split_Lat_Lon[1]);
+                    LatLng chargerLocation = new LatLng(charger_lon, charger_lat); // LatLng of the chargers positions
+
+                    if("l".equals(stateEnds)){
+                        state = "Available";
+                        mMap.addMarker(new MarkerOptions().position(chargerLocation).title(charger_Name).icon(BitmapDescriptorFactory.fromResource(R.drawable.flag)).anchor(0.5f, 1));
+
+
+                    }
+                    else if ("c".equals(stateEnds)){
+                        state = "Occupied";
+                        mMap.addMarker(new MarkerOptions().position(chargerLocation).title(charger_Name).icon(BitmapDescriptorFactory.fromResource(R.drawable.flag)).anchor(0.5f, 1));
+
+
+                    }
+                    else{
+                        state = "Out of Contact";
+                        mMap.addMarker(new MarkerOptions().position(chargerLocation).title(charger_Name).icon(BitmapDescriptorFactory.fromResource(R.drawable.flag)).anchor(0.5f, 1));
+
+                    }
+
+
+                    //.icon(BitmapDescriptorFactory.fromResource(R.drawable.flag)).anchor(0.5f, 1);
+
+
                     line = reader.readLine();
-
                 }
-
-
-            }
-            catch (IOException e)
-            {
+            } catch(IOException ioe){
+                ioe.printStackTrace();
                 Toast.makeText(getApplicationContext(),"Somthing bad happended" , Toast.LENGTH_LONG).show();
-
             }
-
 
 
             }
@@ -231,6 +262,8 @@ public class MapMain extends FragmentActivity implements OnMapReadyCallback, Nav
 
 
     }
+
+
 
 
 
