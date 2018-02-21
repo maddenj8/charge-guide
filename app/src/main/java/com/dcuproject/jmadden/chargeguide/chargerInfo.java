d86f5c334cd6e25f36f58cd0fa55ed2e037ba960
nugenc12 <conor.nugent12@mail.dcu.ie>
2018-02-21 14:02:16 +0000

    added bundel functionalty in chargerinfo

--- app/src/main/java/com/dcuproject/jmadden/chargeguide/chargerInfo.java
+++ app/src/main/java/com/dcuproject/jmadden/chargeguide/chargerInfo.java
@@ -3,6 +3,7 @@ package com.dcuproject.jmadden.chargeguide;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class chargerInfo extends AppCompatActivity {
 
@@ -12,8 +13,20 @@ public class chargerInfo extends AppCompatActivity {
         setContentView(R.layout.activity_charger_info);
 
 
-        Intent receive = getIntent();
-        Bundle bundle = receive.getBundleExtra("lat");
+
+        Bundle bundle = null;
+        bundle = this.getIntent().getExtras();
+
+
+
+
+        double lat = bundle.getDouble("lat");
+        double lon = bundle.getDouble("lon");
+        String chargerTitle = bundle.getString("chargerTitle");
+        String  chargerSnipit = bundle.getString("chargerSnipit");
+        Log.d("lat",lat + "");
+
+
         //just a test to get items from the main class to the about charger class 
     }
 }
