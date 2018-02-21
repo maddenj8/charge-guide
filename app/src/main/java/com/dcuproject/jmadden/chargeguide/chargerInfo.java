67e88228cca28b322789cb020befc8c8f350eb4c
maddenj8 <jason.madden8@mail.dcu.ie>
2018-02-21 13:59:22 +0000

    getting info from infoWindow click

--- app/src/main/java/com/dcuproject/jmadden/chargeguide/chargerInfo.java
+++ app/src/main/java/com/dcuproject/jmadden/chargeguide/chargerInfo.java
@@ -3,6 +3,8 @@ package com.dcuproject.jmadden.chargeguide;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class chargerInfo extends AppCompatActivity {
 
@@ -11,9 +13,14 @@ public class chargerInfo extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_charger_info);
 
+        TextView title = (TextView) findViewById(R.id.heading_charger);
+        TextView subHeading = (TextView) findViewById(R.id.subheading_charger);
+        TextView status = (TextView) findViewById(R.id.status_charger);
+        ImageView icon = (ImageView) findViewById(R.id.status_icon);
 
         Intent receive = getIntent();
         Bundle bundle = receive.getBundleExtra("lat");
+        title.setText(bundle.getString("title"));
         //just a test to get items from the main class to the about charger class 
     }
 }
